24887965b4a46d247c9becf905a8cb9aa53fe8e0
Kingfrankbob <AJWpcc@gmail.com>
2024-06-21 00:48:40 -0500

    Weather and time finished

--- API.py
+++ API.py
@@ -40,36 +40,50 @@ class matrix_api:
         pass
 
     def weather(self):
-        weather_data = get_weather_json()
+        weather_data = self.weather_request.get_weather_json()['data']
         if 'error' in weather_data:
             return
-
-        temp = weather_data['main']['temp']
+        
+        self.color_array.clear()
+        
+        temp = (weather_data['main']['temp'] * 1.8) + 32# 
         description = weather_data['weather'][0]['description']
         clouds = weather_data['clouds']['all']
-        
-        self.color_array.print_text(f"Temp: {temp}°C", 0, 0, [0, 255, 0])
 
+        self.color_array.print_text(f"Wind:{weather_data['wind']['speed']}m/s", 1, 37, [255, 255, 255])
+        self.color_array.print_text(f"h%:{weather_data['main']['humidity']}", 1, 46, [200, 225, 255])
+        self.color_array.print_text(f"FL:{round(weather_data['main']['feels_like'])}", 1, 55, [255, 255, 255])
+
+        self.color_array.draw_wind_dir(30, 37, weather_data['wind']['deg'])
+
+        if(temp < 50):
+            self.color_array.print_text(f"Temp: {round(temp, 1)}", 1, 0, [0, 0, 255])
+        elif(temp < 78):
+            self.color_array.print_text(f"Temp: {round(temp, 1)}", 1, 0, [0, 255, 0])
+        else:
+            self.color_array.print_text(f"Temp: {round(temp, 1)}", 1, 0, [255, 0, 0])
+        
         if 'rain' in description.lower():
             precip_amount = weather_data.get('rain', {}).get('1h', 0)
-            self.color_array.print_text(f"Rain: {precip_amount}mm", 0, 20, [0, 0, 255])  
-            self.color_array.display_icon("rain_icon", 30, 20) 
+            self.color_array.print_text(f"Rain: {precip_amount}mm", 0, 8, [0, 0, 255])  
+            self.color_array.display_icon("rain_icon", 0, 25) 
 
         elif 'snow' in description.lower():
             precip_amount = weather_data.get('snow', {}).get('1h', 0)
-            self.color_array.print_text(f"Snow: {precip_amount}mm", 0, 20, [255, 255, 255]) 
-            self.color_array.display_icon("snow_icon", 30, 20)
+            self.color_array.print_text(f"Snow: {precip_amount}mm", 1, 8, [255, 255, 255]) 
+            self.color_array.display_icon("snow_icon", 5, 18)
 
         elif 'cloud' in description.lower():
-            self.color_array.print_text("Cloudy", 0, 20, [128, 128, 128])  
-            self.color_array.display_icon("cloud_icon", 30, 20)  
+            self.color_array.print_text("Cloudy", 1, 8, [128, 128, 128])  
+            self.color_array.display_icon("cloud_icon", 5, 23)  
 
         else:
-            self.color_array.print_text("Clear Sky", 0, 20, [255, 165, 0])  
-            self.color_array.display_icon("sun_icon", 30, 20) 
+            self.color_array.print_text("Clear Sky", 1, 8, [255, 165, 0])  
+            self.color_array.display_icon("sun_icon", 26, 20)
 
     def time(self):
-        pass
+        self.color_array.clear()
+        self.color_array.draw_clock(0, 0)
 
     def pool(self):
         pass
@@ -78,5 +92,5 @@ class matrix_api:
 if __name__ == '__main__':
     # matrix_api.app.run(debug=True)
     matrix_apii = matrix_api()
-    matrix_apii.weather()
+    matrix_apii.time()
     matrix_apii.color_array.print()
--- matrix.py
+++ matrix.py
@@ -1,3 +1,6 @@
+import math
+from datetime import datetime
+
 class LEDMatrix:
 
     FONT = {
@@ -507,12 +510,12 @@ class LEDMatrix:
         ],
         '0': [
             [0, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0],
+            [0, 1, 1, 1, 0],
             [0, 1, 0, 1, 0],
             [0, 1, 0, 1, 0],
             [0, 1, 0, 1, 0],
             [0, 1, 0, 1, 0],
-            [0, 0, 1, 1, 0]
+            [0, 1, 1, 1, 0]
         ],
         '1': [
             [0, 0, 0, 0, 0],
@@ -594,7 +597,16 @@ class LEDMatrix:
             [0, 0, 0, 1, 0],
             [0, 0, 0, 1, 0],
             [0, 1, 1, 0, 0]
-        ]
+        ],
+        '%':
+         [ [1, 1, 0, 0, 1],
+    [1, 1, 0, 1, 0],
+    [0, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 1, 0, 1, 1],
+    [1, 0, 0, 1, 1]
+         ]
     }
 
     def __init__(self, width, height):
@@ -628,30 +640,94 @@ class LEDMatrix:
                     print('.', end='')
                 else:
                     print('#', end='')
-            print()
+            print("END")
 
     def display_icon(self, icon_name, x, y):
         if icon_name == "sun_icon":
-            self.draw_circle(x + 5, y + 5, 5, [255, 255, 0]) 
+            self.draw_circle(x + 5, y + 5, 5, [255, 255, 0], True) 
+            self.draw_circle(x+5,y+5, 2, [255, 136, 0], True)
             self.draw_line(x + 5, y + 5, x + 5, y + 15, [255, 255, 0])  
             self.draw_line(x + 5, y + 5, x + 15, y + 5, [255, 255, 0])
             self.draw_line(x + 5, y + 5, x - 5, y + 5, [255, 255, 0])
             self.draw_line(x + 5, y + 5, x + 5, y - 5, [255, 255, 0])
+            self.draw_line(x - 4, y - 4, x + 14, y + 14, [255, 255, 0])
+            self.draw_line(x + 14, y - 4, x - 4, y + 14, [255, 255, 0])
 
         elif icon_name == "cloud_icon":
-            self.draw_circle(x + 10, y + 5, 5, [192, 192, 192])
-            self.draw_circle(x + 5, y + 5, 5, [192, 192, 192])
-            self.draw_circle(x, y + 5, 5, [192, 192, 192])
+            self.draw_circle(x + 17, y, 5, [230, 230, 230], True)
+            self.draw_circle(x + 12, y, 5, [230, 230, 230], True)
+            self.draw_circle(x+7, y, 5, [230, 230, 230], True)
+
+            self.draw_circle(x + 10, y + 5, 5, [192, 192, 192], True)
+            self.draw_circle(x + 5, y + 5, 5, [192, 192, 192], True)
+            self.draw_circle(x, y + 5, 5, [192, 192, 192], True)
+
+            self.draw_circle(x + 40, y + 3, 5, [192, 192, 192], True)
+            self.draw_circle(x + 35, y + 3, 5, [192, 192, 192], True)
+            self.draw_circle(x + 30, y + 3, 5, [192, 192, 192], True)
+
+            self.draw_circle(x + 52, y, 5, [255,255,255], True)
+            self.draw_circle(x + 45, y, 5, [255,255,255], True)
+            self.draw_circle(x + 38, y, 5, [255,255,255], True)
 
         elif icon_name == "rain_icon":
-            self.draw_line(x, y, x + 5, y + 5, [0, 0, 255])  
-            self.draw_line(x + 2, y, x + 7, y + 5, [0, 0, 255])
-            self.draw_line(x + 4, y, x + 9, y + 5, [0, 0, 255])
+
+            self.draw_circle(x + 13, y-2, 3, [192, 192, 192], True)
+            self.draw_circle(x + 8, y-2, 3, [192, 192, 192], True)
+            self.draw_circle(x + 3, y-2, 3, [192, 192, 192], True)
+            self.draw_circle(x + 19, y-2, 3, [192, 192, 192], True)
+            self.draw_circle(x + 24, y-2, 3, [192, 192, 192], True)
+            self.draw_circle(x + 29, y-2, 3, [192, 192, 192], True)
+            self.draw_circle(x + 39, y-2, 3, [192, 192, 192], True)
+            self.draw_circle(x + 44, y-2, 3, [192, 192, 192], True)
+            self.draw_circle(x + 49, y-2, 3, [192, 192, 192], True)
+
+
+            self.draw_line(x, y, x+63, y, [0, 0, 0])
+            self.draw_line(x, y+1, x+63, y+1, [0, 0, 0])
+
+
+            self.draw_line(x+1, y, x + 4, y + 5, [0, 0, 255])  
+            self.draw_line(x + 4, y, x + 7, y + 5, [0, 0, 255])
+            self.draw_line(x + 7, y, x + 10, y + 5, [0, 0, 255])
+            self.draw_line(x + 10, y, x + 13, y + 5, [0, 0, 255])
+            self.draw_line(x + 13, y, x + 16, y + 5, [0, 0, 255])
+            self.draw_line(x + 18, y, x + 21, y + 5, [0, 0, 255])
+            self.draw_line(x + 21, y, x + 24, y + 5, [0, 0, 255])
+            self.draw_line(x + 24, y, x + 27, y + 5, [0, 0, 255])
+            self.draw_line(x + 27, y, x + 30, y + 5, [0, 0, 255])
+            self.draw_line(x + 30, y, x + 33, y + 5, [0, 0, 255])
+            self.draw_line(x + 38, y, x + 41, y + 5, [0, 0, 255])
+            self.draw_line(x + 41, y, x + 44, y + 5, [0, 0, 255])
+            self.draw_line(x + 44, y, x + 47, y + 5, [0, 0, 255])
+            self.draw_line(x + 47, y, x + 50, y + 5, [0, 0, 255])
+            self.draw_line(x + 50, y, x + 53, y + 5, [0, 0, 255])
+
 
         elif icon_name == "snow_icon":
-            self.draw_circle(x + 5, y + 5, 3, [255, 255, 255]) 
-            self.draw_circle(x + 10, y + 10, 3, [255, 255, 255])
-            self.draw_circle(x, y + 10, 3, [255, 255, 255])
+            self.draw_circle(x + 4, y + 5, 3, [255, 255, 255], True) 
+            self.draw_circle(x + 9, y + 10, 3, [255, 255, 255])
+            self.draw_circle(x-1, y + 10, 3, [255, 255, 255])
+            self.draw_circle(x + 14, y + 5, 3, [255, 255, 255], True)
+            self.draw_circle(x + 19, y+10, 3, [255, 255, 255], True) 
+            self.draw_circle(x + 24, y + 5, 3, [255, 255, 255])
+            self.draw_circle(x + 29, y + 10, 3, [255, 255, 255], True)
+            self.draw_circle(x + 34, y + 5, 3, [255, 255, 255], True)
+            self.draw_circle(x + 39, y+10, 3, [255, 255, 255])
+            self.draw_circle(x + 44, y + 5, 3, [255, 255, 255])
+            self.draw_circle(x + 49, y + 10, 3, [255, 255, 255])
+            self.draw_circle(x + 54, y + 5, 3, [255, 255, 255], True)
+
+    def draw_wind_dir(self, x, y, angle):
+
+        self.draw_circle(x+16, y+17, 8, [255, 255, 255], True)
+
+        length = 8  
+        end_x = x + 16 + length * math.cos(math.radians(angle))
+        end_y = y + 17 - length * math.sin(math.radians(angle))
+        
+        self.draw_line(x + 16, y + 17, round(end_x), round(end_y), [255, 0, 0])
+
 
     def draw_line(self, x1, y1, x2, y2, color):
         dx = abs(x2 - x1)
@@ -674,30 +750,59 @@ class LEDMatrix:
                 err += dx
                 y1 += sy
 
-    def draw_circle(self, x0, y0, radius, color):
+    def draw_circle(self, x0, y0, radius, color, fill=False):
         x = radius
         y = 0
         err = 0
         
         while x >= y:
-            self.set_pixel(x0 + x, y0 + y, color)
-            self.set_pixel(x0 + y, y0 + x, color)
-            self.set_pixel(x0 - y, y0 + x, color)
-            self.set_pixel(x0 - x, y0 + y, color)
-            self.set_pixel(x0 - x, y0 - y, color)
-            self.set_pixel(x0 - y, y0 - x, color)
-            self.set_pixel(x0 + y, y0 - x, color)
-            self.set_pixel(x0 + x, y0 - y, color)
+            if fill:
+                for i in range(x0 - x, x0 + x + 1):
+                    self.set_pixel(i, y0 + y, color)
+                    self.set_pixel(i, y0 - y, color)
+                for i in range(x0 - y, x0 + y + 1):
+                    self.set_pixel(i, y0 + x, color)
+                    self.set_pixel(i, y0 - x, color)
+            else:
+                self.set_pixel(x0 + x, y0 + y, color)
+                self.set_pixel(x0 + y, y0 + x, color)
+                self.set_pixel(x0 - y, y0 + x, color)
+                self.set_pixel(x0 - x, y0 + y, color)
+                self.set_pixel(x0 - x, y0 - y, color)
+                self.set_pixel(x0 - y, y0 - x, color)
+                self.set_pixel(x0 + y, y0 - x, color)
+                self.set_pixel(x0 + x, y0 - y, color)
             
             y += 1
-            err += 1 + 2*y
-            if 2*(err - x) + 1 > 0:
+            err += 1 + 2 * y
+            if 2 * (err - x) + 1 > 0:
                 x -= 1
-                err += 1 - 2*x
+                err += 1 - 2 * x
+
 
     def set_pixel(self, x, y, color):
         self.matrix[y][x] = color
-        pass
 
     
-
+    def draw_clock(self, x, y):
+        now = datetime.now()
+        hour = now.hour % 12 
+        minute = now.minute
+        
+        self.draw_circle(29, 29, 27, [255, 255, 255], fill=True)
+        self.draw_circle(29, 29, 27, [0, 0, 0])
+        
+        hour_angle = 360 * (hour / 12.0) + (360 / 12.0) * (minute / 60.0)
+        hour_hand_length = 27 // 2
+        hour_hand_end_x = 29 + hour_hand_length * math.cos(math.radians(90 - hour_angle))
+        hour_hand_end_y = 29 - hour_hand_length * math.sin(math.radians(90 - hour_angle))
+        self.draw_line(29, 29, round(hour_hand_end_x), round(hour_hand_end_y), [0, 0, 0])
+        
+        minute_angle = 360 * (minute / 60.0)
+        minute_hand_length = 27 - 3
+        minute_hand_end_x = 29 + minute_hand_length * math.cos(math.radians(90 - minute_angle))
+        minute_hand_end_y = 29 - minute_hand_length * math.sin(math.radians(90 - minute_angle))
+        self.draw_line(29, 29, round(minute_hand_end_x), round(minute_hand_end_y), [0, 0, 0])
+        
+        digital_time = now.strftime("%I:%M %p")
+        self.print_text(digital_time, 9, 57, [255, 255, 255])
