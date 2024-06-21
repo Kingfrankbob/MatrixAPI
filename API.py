912effa5a8bc5644906f48b8a36a401528f532d9
Kingfrankbob <AJWpcc@gmail.com>
2024-06-20 22:29:18 -0500

    More changes due to Stephens ABSOLUTE MEGAMIND

--- API.py
+++ API.py
@@ -1,86 +1,80 @@
 from flask import Flask, request, jsonify
-from flask_restful import Resource, API
-from .matrix import LEDMatrix
-from .weather import get_weather_json
+from flask_restful import Resource, Api
+from matrix import LEDMatrix
+from weather import get_weather_json
 
-app = Flask(__name__)
-api = API(app)
+class matrix_api:
+    app = Flask(__name__)
+    # api = API(app)
 
-width = 64
-height = 64
-color_array = LEDMatrix(width, height)
-current_screen = "weather"
+    def __init__(self):
+        self.width = 64
+        self.height = 64
+        self.color_array = LEDMatrix(self.width, self.height)
+        self.current_screen = "weather"
 
-@app.route('/', methods=['GET'])
-def hello_world():
-        return jsonify({'hello': 'world'}), 200
-    
-@app.route('/frame', methods=['GET'])
-def get_anim_frame():
-    global color_array
-    return jsonify({'frame': color_array.matrix}), 200
+    @app.route('/', methods=['GET'])
+    def hello_world(self):
+            return jsonify({'hello': 'world'}), 200
+        
+    @app.route('/frame', methods=['GET'])
+    def get_anim_frame(self):
+        return jsonify({'frame': self.color_array.matrix}), 200
 
-@app.route('/frame', methods=['POST'])
-def set_anim_frame():
-    global current_screen
-    data = request.get_json()
-    current_screen = data.get('type')
-    update_screen()
-    
-    return jsonify({'message': f'Animation frame for {data.get('type')} set successfully'}), 200 
+    @app.route('/frame', methods=['POST'])
+    def set_anim_frame(self):
+        data = request.get_json()
+        self.current_screen = data.get('type')
+        self.update_screen()
+        return jsonify({'message': f'Animation frame for {data.get('type')} set successfully'}), 200 
 
-def update_screen():
-     
-    global current_screen
+    def update_screen(self):
+        if(self.current_screen == "weather"):
+            self.weather()
+        elif(self.current_screen == "time"):
+            self.time()
+        elif(self.current_screen == "pool"):
+            self.pool()
 
-    if(current_screen == "weather"):
-        weather()
-    elif(current_screen == "time"):
-        time()
-    elif(current_screen == "pool"):
-        pool()
+        pass
 
-    pass
+    def weather(self):
+        weather_data = get_weather_json()
+        if 'error' in weather_data:
+            return
 
-def weather():
-    weather_data = get_weather_json()
-    if 'error' in weather_data:
-          return
-    
-    global color_array
+        temp = weather_data['main']['temp']
+        description = weather_data['weather'][0]['description']
+        clouds = weather_data['clouds']['all']
+        
+        self.color_array.print_text(f"Temp: {temp}°C", 0, 0, [0, 255, 0])
 
-    temp = weather_data['main']['temp']
-    description = weather_data['weather'][0]['description']
-    clouds = weather_data['clouds']['all']
-    
-    color_array.print_text(f"Temp: {temp}°C", 0, 0, [0, 255, 0])
+        if 'rain' in description.lower():
+            precip_amount = weather_data.get('rain', {}).get('1h', 0)
+            self.color_array.print_text(f"Rain: {precip_amount}mm", 0, 20, [0, 0, 255])  
+            self.color_array.display_icon("rain_icon", 30, 20) 
 
-    if 'rain' in description.lower():
-        precip_amount = weather_data.get('rain', {}).get('1h', 0)
-        LEDMatrix.print_text(f"Rain: {precip_amount}mm", 0, 20, [0, 0, 255])  
-        LEDMatrix.display_icon("rain_icon", 30, 20) 
+        elif 'snow' in description.lower():
+            precip_amount = weather_data.get('snow', {}).get('1h', 0)
+            self.color_array.print_text(f"Snow: {precip_amount}mm", 0, 20, [255, 255, 255]) 
+            self.color_array.display_icon("snow_icon", 30, 20)
 
-    elif 'snow' in description.lower():
-        precip_amount = weather_data.get('snow', {}).get('1h', 0)
-        LEDMatrix.print_text(f"Snow: {precip_amount}mm", 0, 20, [255, 255, 255]) 
-        LEDMatrix.display_icon("snow_icon", 30, 20)
+        elif 'cloud' in description.lower():
+            self.color_array.print_text("Cloudy", 0, 20, [128, 128, 128])  
+            self.color_array.display_icon("cloud_icon", 30, 20)  
 
-    elif 'cloud' in description.lower():
-        LEDMatrix.print_text("Cloudy", 0, 20, [128, 128, 128])  
-        LEDMatrix.display_icon("cloud_icon", 30, 20)  
+        else:
+            self.color_array.print_text("Clear Sky", 0, 20, [255, 165, 0])  
+            self.color_array.display_icon("sun_icon", 30, 20) 
 
-    else:
-        LEDMatrix.print_text("Clear Sky", 0, 20, [255, 165, 0])  
-        LEDMatrix.display_icon("sun_icon", 30, 20) 
+    def time(self):
+        pass
 
-    pass
-
-def time():
-     pass
-
-def pool():
-     pass
+    def pool(self):
+        pass
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # matrix_api.app.run(debug=True)
+    matrix_api.weather()
+    matrix_api.color_array.print()
